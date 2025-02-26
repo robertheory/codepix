1e432d1a56b15beb55430d314eae6c5d708343e4
Roberto Costa <roberto.araujo339@gmail.com>
2025-02-26 14:23:31 -0300

    refactor domain models to include GORM tags and improve field definitions

--- codepix-service/domain/model/transaction.go
+++ codepix-service/domain/model/transaction.go
@@ -26,13 +26,15 @@ type Transactions struct {
 }
 
 type Transaction struct {
-	Base
-	AccountFrom       *Account `json:"account_from" valid:"-"`
-	Amount            float64  `json:"amount" valid:"notnull"`
-	PixKeyTo          *PixKey  `json:"pix_key_to" valid:"-"`
-	Status            string   `json:"status" valid:"notnull"`
-	Description       string   `json:"description" valid:"-"`
-	CancelDescription string   `json:"cancel_description" valid:"-"`
+	Base              `valid:"required"`
+	AccountFrom       *Account `valid:"-"`
+	AccountFromID     string   `gorm:"column:account_from_id;type:uuid;" valid:"notnull"`
+	Amount            float64  `json:"amount" gorm:"type:float" valid:"notnull"`
+	PixKeyTo          *PixKey  `valid:"-"`
+	PixKeyIdTo        string   `gorm:"column:pix_key_id_to;type:uuid;" valid:"notnull"`
+	Status            string   `json:"status" gorm:"type:varchar(20)" valid:"notnull"`
+	Description       string   `json:"description" gorm:"type:varchar(255)" valid:"-"`
+	CancelDescription string   `json:"cancel_description" gorm:"type:varchar(255)" valid:"-"`
 }
 
 func (transaction *Transaction) isValid() error {
