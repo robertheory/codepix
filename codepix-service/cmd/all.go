5fac4f9a85b05b885e32279c0026f7fb40a723e0
Roberto Costa <roberto.araujo339@gmail.com>
2025-02-28 10:54:58 -0300

    feat: add command to run all services with gRPC and Kafka integration

--- codepix-service/cmd/all.go
+++ codepix-service/cmd/all.go
@@ -6,6 +6,7 @@ package cmd
 import (
 	"os"
 
+	"github.com/robertheory/codepix/app/grpc"
 	"github.com/robertheory/codepix/app/kafka"
 	"github.com/robertheory/codepix/infra/db"
 	"github.com/spf13/cobra"
@@ -13,36 +14,39 @@ import (
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-// kafkaCmd represents the kafka command
-var kafkaCmd = &cobra.Command{
-	Use:   "kafka",
-	Short: "Start consuming transactions using Apache Kafka",
+var grpcPortNumber int
+
+// allCmd represents the all command
+var allCmd = &cobra.Command{
+	Use:   "all",
+	Short: "Run all services",
+	Long:  `Command to run all services of the application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		deliveryChan := make(chan ckafka.Event)
 
 		database := db.ConnectDB(os.Getenv("env"))
 
+		go grpc.StartGrpcServer(database, grpcPortNumber)
+
+		deliveryChan := make(chan ckafka.Event)
 		producer := kafka.NewKafkaProducer()
-
-		// kafka.Publish("Hello Kafka", "test", producer, deliveryChan)
-
 		go kafka.DeliveryReport(deliveryChan)
-
 		kafkaProcessor := kafka.NewKafkaProcessor(database, producer, deliveryChan)
 		kafkaProcessor.Consume()
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(kafkaCmd)
+	rootCmd.AddCommand(allCmd)
+
+	allCmd.Flags().IntVarP(&grpcPortNumber, "port", "p", 50051, "gRPC server port")
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// kafkaCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// allCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// kafkaCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// allCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- codepix-service/go.mod
+++ codepix-service/go.mod
@@ -14,14 +14,19 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	golang.org/x/net v0.32.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
@@ -31,6 +36,7 @@ require (
 require (
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/confluentinc/confluent-kafka-go v1.9.2
+	github.com/go-playground/validator/v10 v10.25.0
 	github.com/stretchr/testify v1.10.0
 	google.golang.org/grpc v1.70.0
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
